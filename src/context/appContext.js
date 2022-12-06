0354d86eae16d1e8e669729a038a013196b198f9
Netanel Alroy <netanelal100@gmail.com>
2022-12-06 23:26:10 +0200

    Register/ login  && landing page completed

---
+++ src/context/appContext.js
@@ -0,0 +1,30 @@
+import { DISPLAY_ALERT } from "./actions"
+
+import reducer from './reducer'
+import React, {useReducer, useContext} from 'react'
+const initialState = {
+    isLoading: false,
+    showAlert: false,
+    alertText: '',
+    alertType: '',
+}
+
+const AppContext = React.createContext()
+
+const AppProvider = ({children}) => {
+    const [state, dispatch] = useReducer(reducer,initialState)
+    const displayAlert = () => { 
+        dispatch({type: DISPLAY_ALERT})
+    }
+    return (
+        <AppContext.Provider value={{...state, displayAlert}}>{children}
+
+        </AppContext.Provider>
+    )
+}
+
+const useAppContext = () => { 
+    return useContext(AppContext)
+}
+
+export {AppProvider, initialState, useAppContext}
---
+++ src/pages/Landing.js
@@ -0,0 +1,26 @@
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
+import { Link } from 'react-router-dom'
+const Landing = () => {
+  return (
+<Wrapper>
+    <nav>
+        <Logo/>
+    </nav>
+    <div className='container page'>
+        {/* info */}
+    <div className="info">
+    <h1>job <span>tracking</span> app</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ipsa delectus ad nisi voluptatibus in odio eaque. Fuga error voluptatum, perspiciatis minima delectus, libero molestias esse ut laborum est praesentium?</p>
+    <Link to='/register' className='btn btn-hero'>Login/Register
+    </Link>
+    </div>
+    <img src={main} alt="job hunt" className='img main-img'/>
+    </div>
+    
+</Wrapper>  )
+}
+
+
+export default Landing
